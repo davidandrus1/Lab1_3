b3e44f826eb3a10310f5284a7e714215b91b5a93
davidandrus1 <david.andrus@stud.ubbcluj.ro>
2025-10-20 19:16:05 +0300

    Diferenta

--- src/Dif.java
+++ src/Dif.java
@@ -1,2 +1,20 @@
 public class Dif {
+    public static int[] dif(int[] array1, int[] array2){
+        int localDif = 0;
+        int[] result = new int[array1.length];
+        int carry = 0;
+
+        for(int i = array1.length - 1; i >= 0; i--){
+            localDif = array1[i] - array2[i] - carry;
+            if(localDif < 0){
+                localDif+=10;
+                carry = 1;
+            }
+            else{
+                carry = 0;
+            }
+            result[i] = localDif;
+        }
+        return result;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,14 @@ import java.util.Arrays;
 public class Main{
     public static void main(String[] args) {
         Sum suma = new Sum();
+        Dif diference = new Dif();
         int[] array1 = {8, 7, 0, 0, 0, 0, 0, 0, 0};
         int[] array2 = {1, 3, 0, 0, 0, 0, 0, 0, 0};
 
-        int[] result = Sum.sum(array1, array2);
-        System.out.printf(Arrays.toString(result));
+        int[] result1 = Sum.sum(array1, array2);
+        int[] result2 = Dif.dif(array1, array2);
+        System.out.println(Arrays.toString(result1));
+        System.out.println(Arrays.toString(result2));
     }
 
 }
